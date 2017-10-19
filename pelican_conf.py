b4a76c126f4caf85d5a71a011c2f3c67cb3067b6
Gerard Assayag <gerard.assayag@ircam.fr>
2017-10-20 01:11:09 +0200

    Update pelican_conf.py

--- pelican_conf.py
+++ pelican_conf.py
@@ -12,7 +12,7 @@ THEME = '/srv/lib/pelican-themes/pelican-bootstrap3'
 
 #BOOTSTRAP_THEME = 'united'
 #BOOTSTRAP_THEME = 'yeti'
-BOOTSTRAP_THEME = 'lumen'
+BOOTSTRAP_THEME = 'paper'
 # CUSTOM_CSS = 'themes/bootswatch/slate/slate/bootstrap.css'
 
 PATH = '/var/in'
