b28c33922b476b50a1a34ee9613d794ad1cdf58e
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2023-03-06 22:48:29 +0100

    update url and path

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 SITENAME = "Improtech"
-SITEURL = 'https://improtech.ircam.fr'
+SITEURL = 'https://improtech.ircam.fr/ikuzeste'
 # AUTHOR = 'ikPP'
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
@@ -11,7 +11,7 @@ THEME = '/srv/lib/pelican-themes/pelican-bootstrap3'
 
 
 INPUT_PATH = '/var/in'
-OUTPUT_PATH = '/var/out/master'
+OUTPUT_PATH = '/var/out/ikuzeste'
 STATIC_PATHS = ['doc', 'images']
 
 #BOOTSTRAP_THEME = 'united'
